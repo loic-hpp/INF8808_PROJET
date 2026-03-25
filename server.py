5a58a47de01fe671f05e1ba9c2ff2dd8a78fbce4
loic-hpp <loic.nguemegne-temena@polymtl.ca>
2026-03-24 22:41:01 -0400

    add test files to validate pipeline

---
+++ server.py
@@ -0,0 +1,23 @@
+'''
+    Contains the server to run our application.
+'''
+import os
+from flask_failsafe import failsafe
+
+
+@failsafe
+def create_app():
+    '''
+        Gets the underlying Flask server from our Dash app.
+
+        Returns:
+            The server to be run
+    '''
+    # the import is intentionally inside to work with the server failsafe
+    from app import app  # pylint: disable=import-outside-toplevel
+    return app.server
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8050))
+    create_app().run(host="0.0.0.0", port=port, debug=False)
